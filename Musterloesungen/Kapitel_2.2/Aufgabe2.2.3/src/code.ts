4639bf5631a29394d86b6321efdf405b0116c81f
Rene Pilz <you@example.com>
2025-09-06 09:16:29 +0200

    Aufgabe 2.2.3

---
+++ Musterloesungen/Kapitel_2.2/Aufgabe2.2.3/src/code.ts
@@ -0,0 +1,20 @@
+interface Kurs {
+    id: number;
+    name: string;
+    dozent: string;
+}
+
+const wt1: Kurs = {
+    id: 1,
+    name: "Webtechnologien 1",
+    dozent: "Rene Pilz",
+}
+
+const wt2: Kurs = {
+    id: 2,
+    name: "Webtechnologien 2",
+    dozent: "Rene Pilz",
+}
+
+console.log("Kurs "+wt1.id+": " + wt1.name + " mit " + wt1.dozent);
+console.log("Kurs "+wt2.id+": " + wt2.name + " mit " + wt2.dozent);
