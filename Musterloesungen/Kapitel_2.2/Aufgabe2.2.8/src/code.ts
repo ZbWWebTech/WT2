8391adb43a9b86d9b244ba55a80b1151965a878b
Rene Pilz <you@example.com>
2026-05-15 15:34:07 +0200

    Aufgabe 2.2.8 (Events)

---
+++ Musterloesungen/Kapitel_2.2/Aufgabe2.2.8/src/code.ts
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+    const liste = document.getElementById("liste");
+    liste?.addEventListener("click", (event) => {
+        const target = event.target as HTMLElement;
+        let selectedItem = target;
+        while (selectedItem &&selectedItem.tagName !== "LI") {
+            selectedItem = selectedItem.parentElement as HTMLElement;
+        }
+        if (selectedItem.tagName === "LI") {
+            console.log(selectedItem);
+        }
+    });
+});
