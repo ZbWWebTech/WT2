4c56d90a707422b5712be2b0fedcb4fcfa6ef83a
Rene Pilz <pilz@pullman-group.com>
2026-05-16 14:24:53 +0200

    typ weiter definiert

--- Musterloesungen/Kapitel_2.2/Aufgabe2.2.8/src/code.ts
+++ Musterloesungen/Kapitel_2.2/Aufgabe2.2.8/src/code.ts
@@ -2,7 +2,7 @@ window.addEventListener("load", () => {
     const liste = document.getElementById("liste");
     liste?.addEventListener("click", (event) => {
         const target = event.target as HTMLElement;
-        let selectedItem = target;
+        let selectedItem : HTMLElement= target;
         while (selectedItem &&selectedItem.tagName !== "LI") {
             selectedItem = selectedItem.parentElement as HTMLElement;
         }
